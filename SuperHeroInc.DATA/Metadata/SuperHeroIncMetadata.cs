8b412046330ad64c4bb42403a3077f45600347ee
KPLap <kamkperry@outlook.com>
2017-08-17 11:29:21 -0500

    004: Database created, references in.UI to .DATA made. Template conversion still in progress.  Base roles for citizen, Hero and Admin created.

---
+++ SuperHeroInc.DATA/Metadata/SuperHeroIncMetadata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroInc.DATA.Metadata
+{
+    class SuperHeroIncMetadata
+    {
+    }
+}
